1362708f9f888b14b8f363a79acda9ee49c65465
Greg McCabe <gsmccabe1@gmail.com>
2014-06-06 09:44:58 -0500

    formating todo list with css

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
 
 <?php
-var_dump($_FILES);
+// var_dump($_FILES);
 
 $filename = 'data/title.txt';
 $todos_array = read_file($filename);
@@ -9,11 +8,11 @@ $todos_array = read_file($filename);
 function read_file($filename) {
     $handle = fopen($filename, "r");
     if(filesize($filename) > 0) {
-    	$contents = trim(fread($handle, filesize($filename))); // contents = string
-    	$contents_array = explode("\n", $contents);
+        $contents = trim(fread($handle, filesize($filename))); // contents = string
+        $contents_array = explode("\n", $contents);
 
     } else {
-    	$contents_array = array();
+        $contents_array = array();
     }
 
     fclose($handle);
@@ -26,39 +25,39 @@ function write_file_save($filename, $items) {
 }
 
 if (!empty($_POST["input_item"])) {
-	array_push($todos_array, $_POST["input_item"]);
-	write_file_save($filename, $todos_array);
-	header('Location: /todo_list.php');
-	exit;
+    array_push($todos_array, $_POST["input_item"]);
+    write_file_save($filename, $todos_array);
+    header('Location: /todo_list.php');
+    exit;
 }
 if (isset($_GET['removeIndex'])) {
-	unset($todos_array[$_GET['removeIndex']]);
-	write_file_save($filename, $todos_array);
-	header('Location: /todo_list.php');
-	exit;
+    unset($todos_array[$_GET['removeIndex']]);
+    write_file_save($filename, $todos_array);
+    header('Location: /todo_list.php');
+    exit;
 }
 
 // Verify there were uploaded files and no errors
 if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
 
-	if ($_FILES['file1']["type"] != "text/plain") {
-		echo "ERROR: file must be in text/plain!";
-	} else {
-		// Set the destination directory for uploads
-	    // Grab the filename from the uploaded file by using basename
-	    $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
-	    $uploadFilename = basename($_FILES['file1']['name']);
-	    // Create the saved filename using the file's original name and our upload directory
-	    $saved_filename = $upload_dir . $uploadFilename;
-	    // Move the file from the temp location to our uploads directory
-	    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+    if ($_FILES['file1']["type"] != "text/plain") {
+        echo "ERROR: file must be in text/plain!";
+    } else {
+        // Set the destination directory for uploads
+        // Grab the filename from the uploaded file by using basename
+        $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+        $uploadFilename = basename($_FILES['file1']['name']);
+        // Create the saved filename using the file's original name and our upload directory
+        $saved_filename = $upload_dir . $uploadFilename;
+        // Move the file from the temp location to our uploads directory
+        move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
 
-	    // load the new todos
-	    // merge with existing list
-	    $todos_uploaded = read_file($saved_filename);
-	    $todos_array = array_merge($todos_array, $todos_uploaded);
-	    write_file_save($filename, $todos_array);
-	}
+        // load the new todos
+        // merge with existing list
+        $todos_uploaded = read_file($saved_filename);
+        $todos_array = array_merge($todos_array, $todos_uploaded);
+        write_file_save($filename, $todos_array);
+    }
 }
 
 // Check if we saved a file
@@ -70,22 +69,25 @@ if (isset($saved_filename)) {
 ?>
 
 
+<!DOCTYPE html>
 <html>
 <head>
 	<title>"TODO List"</title>
+    <link rel="stylesheet" type="text/css" href="todo_style.css">
 </head>
-
-	<body>
-		<h1>ToDo List:</h1>
-		<ul>
-
+    <div>
+	<body class="background">
+		<h1 id="fancy-header">ToDo List:</h1>
+		<div>
+        <ul>
 		  <? foreach ($todos_array as $key => $value) : ?>
-        	<li><?= htmlspecialchars(strip_tags($value)); ?><a href="todo_list.php?removeIndex=<?= $key?>">Remove Item</a></li>
+        	<li><?= htmlspecialchars(strip_tags($value)); ?> | <a href="todo_list.php?removeIndex=<?= $key?>">Remove Item</a></li>
   			<? endforeach; ?>
 
 		</ul>
+    </div>
 			<br>
-		<h3>Add item to your List</h3>
+		<h1 class="addItem">Add item to your List</h1>
 		<form method="POST" action="/todo_list.php">
 			<p>
 				<label for="input_item">Enter Item</label>
@@ -98,7 +100,8 @@ if (isset($saved_filename)) {
 		<h1>Upload File</h1>
 
 		<form method="POST" enctype="multipart/form-data" action="/todo_list.php">
-	    	<p>
+
+            <p>
 	        	<label for="file1">File to upload: </label>
 	        	<input type="file" id="file1" name="file1">
 	    	</p>
@@ -106,7 +109,7 @@ if (isset($saved_filename)) {
 	        <p><input type="submit" value="Upload"></p>
 
 		</form>
-
+    </div>
 	</body>
 </html>
 
