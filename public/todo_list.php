c66fb5c772557fc7a1441c0470e919b1bef463e1
Greg McCabe <gsmccabe1@gmail.com>
2014-06-20 17:18:50 -0500

    todo list add exceptions try and catch

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,38 +1,32 @@
 
 <?php
 // var_dump($_FILES);
+require('filestore.php');
+$open = new Filestore('data/title.txt');
+// var_dump($_FILES);
+// $filename = 'data/title.txt';
+$todos_array = $open->read();
 
-$filename = 'data/title.txt';
-$todos_array = read_file($filename);
+class UnexpectedTypeException extends Exception { }
 
-function read_file($filename) {
-    $handle = fopen($filename, "r");
-    if(filesize($filename) > 0) {
-        $contents = trim(fread($handle, filesize($filename))); // contents = string
-        $contents_array = explode("\n", $contents);
-
-    } else {
-        $contents_array = array();
+try {
+    if (isset($_POST["input_item"])) {
+        if ($_POST["input_item"] == "" || strlen($_POST["input_item"] > 240)) {
+            throw new UnexpectedTypeException('input_item must be 240 characters or less');
+        }
+        array_push($todos_array, $_POST["input_item"]);
+        $open->write($todos_array);
+        header('Location: /todo_list.php');
+        exit;
     }
 
-    fclose($handle);
-    return $contents_array;
-}
-function write_file_save($filename, $items) {
-    $handle = fopen($filename, 'w');
-    fwrite($handle, implode("\n", $items));
-    fclose($handle);
+} catch (UnexpectedTypeException $e) {
+    $msg = $e->getMessage() . PHP_EOL;
 }
 
-if (!empty($_POST["input_item"])) {
-    array_push($todos_array, $_POST["input_item"]);
-    write_file_save($filename, $todos_array);
-    header('Location: /todo_list.php');
-    exit;
-}
 if (isset($_GET['removeIndex'])) {
     unset($todos_array[$_GET['removeIndex']]);
-    write_file_save($filename, $todos_array);
+    $open->write($todos_array);
     header('Location: /todo_list.php');
     exit;
 }
@@ -54,16 +48,18 @@ if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
 
         // load the new todos
         // merge with existing list
-        $todos_uploaded = read_file($saved_filename);
+        $import = new Filestore("uploads/$uploadFilename");
+        $todos_uploaded = $import->read();
         $todos_array = array_merge($todos_array, $todos_uploaded);
-        write_file_save($filename, $todos_array);
+        $open->write($todos_array);
+        // var_dump($todos_array);
     }
 }
 
 // Check if we saved a file
 if (isset($saved_filename)) {
     // If we did, show a link to the uploaded file
-    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+    echo "<p>You can download your file <a href='/{$import->filename}'>here</a>.</p>";
 }
 
 ?>
@@ -88,6 +84,9 @@ if (isset($saved_filename)) {
     </div>
 			<br>
 		<h1 class="addItem">Add item to your List</h1>
+            <? if (isset($msg)) : ?>
+                <?="Sorry, your input should be greater than 1 character and less than 240 characters"; ?>
+            <? endif; ?>
 		<form method="POST" action="/todo_list.php">
 			<p>
 				<label for="input_item">Enter Item</label>
