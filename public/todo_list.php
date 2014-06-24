2103e0fa2f64a3b799a29f471728125619d1384b
Greg McCabe <gsmccabe1@gmail.com>
2014-06-24 14:04:49 -0500

    1st commit before refactor for db

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,10 +1,9 @@
 
 <?php
-// var_dump($_FILES);
+
 require('filestore.php');
 $open = new Filestore('data/title.txt');
-// var_dump($_FILES);
-// $filename = 'data/title.txt';
+
 $todos_array = $open->read();
 
 class UnexpectedTypeException extends Exception { }
@@ -31,34 +30,28 @@ if (isset($_GET['removeIndex'])) {
     exit;
 }
 
-// Verify there were uploaded files and no errors
+
 if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
 
     if ($_FILES['file1']["type"] != "text/plain") {
         echo "ERROR: file must be in text/plain!";
     } else {
-        // Set the destination directory for uploads
-        // Grab the filename from the uploaded file by using basename
+
         $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
         $uploadFilename = basename($_FILES['file1']['name']);
-        // Create the saved filename using the file's original name and our upload directory
         $saved_filename = $upload_dir . $uploadFilename;
-        // Move the file from the temp location to our uploads directory
         move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
 
-        // load the new todos
-        // merge with existing list
         $import = new Filestore("uploads/$uploadFilename");
         $todos_uploaded = $import->read();
         $todos_array = array_merge($todos_array, $todos_uploaded);
         $open->write($todos_array);
-        // var_dump($todos_array);
+
     }
 }
 
-// Check if we saved a file
+
 if (isset($saved_filename)) {
-    // If we did, show a link to the uploaded file
     echo "<p>You can download your file <a href='/{$import->filename}'>here</a>.</p>";
 }
 
